5f50b588a143e4f8882281b9c5e9b60687576bad
Robert Baufeld <rbaufeld@posteo.de>
2024-11-29 13:28:30 +0100

    installed Jest, React Router and created components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import fetchBestReddits from "./utils/redditAPI";
+import fetchSearchQuery from "./utils/redditAPI";
+import { Listing } from "./utils/redditAPI";
+
 
 function App() {
-  const [data, setData] = useState();
+  const [searchResponse, setSearchResponse] = useState<Listing[] | null>(null);
+  const [query, setQuery] = useState<string>("");
 
-  async function handleFetch() {
-    const fetch = await fetchBestReddits();
-    setData(fetch);
-    console.log("Data: ", data);
+  async function handleFetch(event: React.FormEvent) {
+    event?.preventDefault();
+    const fetch = await fetchSearchQuery(query);
+    setSearchResponse(fetch);
   }
+
+  useEffect(() => {
+    if (searchResponse) {
+      console.log("Data: ", searchResponse);
+    }
+  }, [searchResponse])
 
   return (
     <>
       <h1>Hello World!</h1>
-      <button onClick={handleFetch}>Fetch</button>
+      <input
+        type="text"
+        name="query"
+        id="query"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button type="submit" onClick={handleFetch}>
+        Fetch
+      </button>
+      <section>
+        {searchResponse && searchResponse.map((child) => (
+          <div key={child.data.id}>
+            <div>{child.data.display_name}</div>
+            <div>{child.data.public_description}</div>
+          </div>
+        ))}
+      </section>
     </>
   );
 }
--- src/utils/redditAPI.ts
+++ src/utils/redditAPI.ts
@@ -1,16 +1,30 @@
-const url = "https://www.reddit.com/r/"
+export interface Listing {
+  data: {
+    id: string;
+    display_name: string;
+    public_description: string;
+  };
+}
 
-export default async function fetchBestReddits() {
+export default async function fetchSearchQuery(
+  query: string
+): Promise<Listing[]> {
+  const baseUrl = "https://www.reddit.com/subreddits/search.json";
   try {
-    const response = await fetch(`${url}best.json`, {
-      method: "GET"
-    });
+    const response: Response = await fetch(
+      `${baseUrl}?q=${encodeURI(query)}&limit=25&sort=relevance`,
+      {
+        method: "GET",
+      }
+    );
     if (response.ok) {
       const data = await response.json();
-      console.log(data)
-      return data;
+      return data.data.children;
+    } else {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
   } catch (error) {
-    console.error("The following error has occured: ", error)
+    console.error("The following error has occured: ", error);
+    return []; // Promise Type throws an error if I remove this
   }
 }
