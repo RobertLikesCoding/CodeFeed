56e6e01b26cd32eef5a32e0a7b683f0edd660ac3
Robert Baufeld <rbaufeld@posteo.de>
2024-11-29 15:01:32 +0100

    set up routes and re-implemented the Router differently

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Latest from "./pages/Latest";
-import Frontend from "./pages/Frontend";
-import Backend from "./pages/Backend";
-import Fullstack from "./pages/Fullstack";
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
-
 
 const App: React.FC = () => {
   return (
     <>
-      <BrowserRouter>
-        <header>
-          <nav>
-            <ul>
-              <li className=""><Link to="/">Latest</Link></li>
-              <li className=""><Link to="frontend">Frontend</Link></li>
-              <li className=""><Link to="backend">Backend</Link></li>
-              <li className=""><Link to="fullstack">FullStack</Link></li>
-            </ul>
-          </nav>
-        </header>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route index element={<Latest />} />
-            <Route path="frontend" element={<Frontend />} />
-            <Route path="backend" element={<Backend />} />
-            <Route path="fullstack" element={<Fullstack />} />
-          </Routes>
-      </BrowserRouter>
+      <header>
+        <nav>
+          <ul>
+            <li className="">
+              <Link to="/">Latest</Link>
+            </li>
+            <li className="">
+              <Link to="frontend">Frontend</Link>
+            </li>
+            <li className="">
+              <Link to="backend">Backend</Link>
+            </li>
+            <li className="">
+              <Link to="fullstack">FullStack</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer></footer>
     </>
   );
-}
+};
 
 export default App;
