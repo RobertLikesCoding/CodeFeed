4a53a2b41a39678b648bea4077be13f8d483c590
Robert Baufeld <rbaufeld@posteo.de>
2024-12-02 09:08:07 +0100

    wrote test but missing babel configuration

--- src/router.test.tsx
+++ src/router.test.tsx
@@ -1,5 +1,15 @@
-import { Router } from "react-router-dom";
+import Layout from "./components/Layout"
+import { screen, render, fireEvent } from "@testing-library/react"
+import '@testing-library/jest-dom';
 
-describe("Router", () => {
-  
+describe("Layout", () => {
+  test("should render Frontend heading when clicking frontend tab", () => {
+    render(<Layout />);
+
+    const frontendTab = screen.getByText("Frontend");
+    fireEvent.click(frontendTab);
+    const heading = screen.getByRole("h1");
+
+    expect(heading).toBeInTheDocument();
+  })
 })
