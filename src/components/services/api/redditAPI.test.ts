4d44af86db69e32213c96123df417ed321568f78
Robert Baufeld <rbaufeld@posteo.de>
2024-12-05 14:12:47 +0100

    wrote test for fetching functionality in redditAPI.js

--- src/components/services/api/redditAPI.test.ts
+++ src/components/services/api/redditAPI.test.ts
@@ -1,15 +1,42 @@
 // import { render, screen } from "@testing-library/react"
+import { fetchSearchQuery } from "./redditAPI";
 
-// // test if the api is fetching correccctly
-// // test if the list is being displayed
+// test if the api is fetching correccctly
+// test if the list is being displayed
 
-// describe("Fetch from Reddit", () => {
-//   test("should return a list of posts", () => {
-//     // arrange
+const mockPostsArray = {
+  kind: "t3",
+  data: {
+    children: [
+      {
+        id: "1",
+        author: "test_author",
+        created: 1620000000,
+        num_comments: 10,
+        url: "https://www.example.com",
+        subreddit_name_prefixed: "r/test",
+        title: "Test Post",
+        ups: 100,
+        public_description: "This is a test post description",
+      },
+    ],
+  },
+};
 
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    status: 200,
+    json: () => Promise.resolve(mockPostsArray),
+  })
+) as jest.Mock;
 
-//     // act
+describe("Fetch from Reddit", () => {
+  test("should return a list of posts", async () => {
+    const posts = await fetchSearchQuery("test");
 
-//     // assert
-//   })
-// })
+    expect(posts).toEqual(mockPostsArray.data.children);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+});
