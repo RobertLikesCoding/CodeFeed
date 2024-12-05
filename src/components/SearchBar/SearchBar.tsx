cc28fe7e2519267c0641e1c4899c56d6ecc5acd0
Robert Baufeld <rbaufeld@posteo.de>
2024-12-05 17:07:26 +0100

    installed fontawesome and added Icon

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,8 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
 const SearchBar = () => {
   return (
     <div>
       <input type="text" name="query" id="query" placeholder="e.g. 'React'" aria-label="search query"/>
-      
+      <FontAwesomeIcon icon={faMagnifyingGlass} />
     </div>
   )
 }
