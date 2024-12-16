4b29050bea3ff41705ce5f7151aed153641f4960
Robert Baufeld <rbaufeld@posteo.de>
2024-12-16 14:03:01 +0100

    added Thunk function for async operation

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,33 +3,18 @@ import { AppDispatch, RootState } from "../redux/store";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import PostsList from "../components/Posts/PostsList";
-import { fetchSearchQuery } from "../components/services/api/redditAPI";
-import { setPosts, setIsLoading } from "../redux/querySearch/querySearchSlice";
+import { fetchPostsThunk } from "../redux/querySearch/querySearchSlice";
 
 const MainPage = () => {
   const { topic } = useParams();
-  const posts = useSelector((state: RootState) => state.searchResult.value)
+  const posts = useSelector((state: RootState) => state.searchResult.posts)
   const isLoading = useSelector((state: RootState) => state.searchResult.isLoading)
+  // const hasError = useSelector((state: RootState) => state.searchResult.hasError)
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      dispatch(setIsLoading(true));
-      try {
-        const data = topic
-          ? await fetchSearchQuery(topic)
-          : await fetchSearchQuery("web+development");
-        dispatch(setPosts(data));
-      } catch (error) {
-        console.error("Error fetching posts: ", error);
-        dispatch(setPosts([]));
-      } finally {
-        dispatch(setIsLoading(false));
-      }
-    };
-
-    fetchPosts();
-  }, [topic]);
+    dispatch(fetchPostsThunk(topic))
+  }, [dispatch, topic]);
 
   return (
     <section>
--- src/redux/querySearch/querySearchSlice.ts
+++ src/redux/querySearch/querySearchSlice.ts
@@ -1,30 +1,63 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Post } from "../../components/services/api/redditAPI";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  Post,
+  fetchSearchQuery,
+} from "../../components/services/api/redditAPI";
 
 interface querySearchState {
-  value: Post[];
+  posts: Post[];
   isLoading: boolean;
+  hasError: boolean;
 }
 
 const initialState: querySearchState = {
-  value: [],
+  posts: [],
   isLoading: false,
+  hasError: false,
 };
+
+export const fetchPostsThunk = createAsyncThunk(
+  "searchResult/fetchPosts",
+  async (topic: string | undefined, { rejectWithValue }) => {
+    try {
+      const data = topic
+        ? await fetchSearchQuery(topic)
+        : await fetchSearchQuery("web+development");
+      return data;
+    } catch {
+      return rejectWithValue("Failed to fetch posts");
+    }
+  }
+);
 
 const querySearchSlice = createSlice({
   name: "searchResult",
   initialState,
   reducers: {
     setPosts: (state, action) => {
-      state.value = action.payload
+      state.posts = action.payload;
     },
     setIsLoading: (state, action: PayloadAction<boolean>) => {
       state.isLoading = action.payload;
+    },
+    setHasError: (state) => {
+      state.hasError = !state.hasError;
     }
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPostsThunk.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchPostsThunk.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.posts = action.payload;
+      })
+      .addCase(fetchPostsThunk.rejected, (state) => {
+        state.isLoading = false;
+        state.hasError = true;
+      })
+  }
 });
 
-
-
-export const { setPosts, setIsLoading } = querySearchSlice.actions;
 export default querySearchSlice.reducer;
