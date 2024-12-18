5c617d109134b302b4eefcc8efe671a96cf66d3f
Robert Baufeld <rbaufeld@posteo.de>
2024-12-18 22:53:11 +0100

    refactored fetch Test suite

--- src/components/services/api/redditAPI.test.ts
+++ src/components/services/api/redditAPI.test.ts
@@ -4,86 +4,31 @@ import {
   fetchSubredditPosts,
   fetchSubredditInfo,
 } from "./redditAPI";
-import { mockPosts, mockSubreddits, mockSubredditAbout } from "../../../__mocks__/redditAPI.mock";
+import {
+  mockPosts,
+  mockSubreddits,
+  mockSubredditAbout,
+} from "../../../__mocks__/redditAPI.mock";
 
-describe("Fetch Posts", () => {
+const mockFetch = (mockElement: unknown) => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve(mockElement),
+    })
+  ) as jest.Mock;
+};
+
+describe("Reddit API Services", () => {
   beforeEach(() => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        ok: true,
-        status: 200,
-        json: () => Promise.resolve(mockPosts),
-      })
-    ) as jest.Mock;
+    jest.clearAllMocks();
   });
 
-  test("should return a listing of posts", async () => {
-    const posts = await fetchSearchQuery("test");
-
-    expect(posts).toEqual(mockPosts.data.children);
-    expect(fetch).toHaveBeenCalledTimes(1);
-  });
-
-  test("handles exceptions with console.error", async () => {
-    (fetch as jest.Mock).mockImplementationOnce(() =>
-      Promise.reject("API failure")
-    );
-    const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation();
-    const posts = await fetchSearchQuery("test");
-
-    expect(posts).toEqual([]);
-    expect(consoleErrorSpy).toHaveBeenCalledWith(
-      "Failed to search for posts: ",
-      "API failure"
-    );
-  });
-});
-
-describe("Fetch Subreddits", () => {
-  beforeEach(() => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        ok: true,
-        status: 200,
-        json: () => Promise.resolve(mockSubreddits),
-      })
-    ) as jest.Mock;
-  });
-
-  test("should return a listing of subreddits", async () => {
-    const subreddits = await fetchSubreddits("test");
-
-    expect(subreddits).toEqual(mockSubreddits.data.children);
-    expect(fetch).toHaveBeenCalledTimes(1);
-  });
-
-  test("handles exceptions with console.error", async () => {
-    (fetch as jest.Mock).mockImplementationOnce(() =>
-      Promise.reject("API failure")
-    );
-    const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation();
-    const posts = await fetchSubreddits("test");
-
-    expect(posts).toEqual([]);
-    expect(consoleErrorSpy).toHaveBeenCalledWith(
-      "Couldn't fetch Subreddits: ",
-      "API failure"
-    );
-  });
-
-  describe("Fetch Subreddit Posts", () => {
-    beforeEach(() => {
-      global.fetch = jest.fn(() =>
-        Promise.resolve({
-          ok: true,
-          status: 200,
-          json: () => Promise.resolve(mockPosts),
-        })
-      ) as jest.Mock;
-    });
-
-    test("should return a listing of posts for a subreddit", async () => {
-      const posts = await fetchSubredditPosts("test");
+  describe("Fetch Posts", () => {
+    test("should return a listing of posts", async () => {
+      mockFetch(mockPosts);
+      const posts = await fetchSearchQuery("test");
 
       expect(posts).toEqual(mockPosts.data.children);
       expect(fetch).toHaveBeenCalledTimes(1);
@@ -94,36 +39,23 @@ describe("Fetch Subreddits", () => {
         Promise.reject("API failure")
       );
       const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation();
-      const posts = await fetchSubredditPosts("test");
+      const posts = await fetchSearchQuery("test");
 
       expect(posts).toEqual([]);
       expect(consoleErrorSpy).toHaveBeenCalledWith(
-        "Couldn't fetch Subreddit Posts: ",
+        "Failed to search for posts: ",
         "API failure"
       );
     });
   });
 
-  describe("Fetch Subreddit Info", () => {
-    beforeEach(() => {
-      global.fetch = jest.fn();
-    });
+  describe("Fetch Subreddits", () => {
+    test("should return a listing of subreddits", async () => {
+      mockFetch(mockSubreddits);
+      const subreddits = await fetchSubreddits("test");
 
-    test("should return an info object for a subreddit", async () => {
-      (fetch as jest.Mock).mockResolvedValueOnce({
-        ok: true,
-        json: async () => ({
-          data: mockSubredditAbout,
-        }),
-      });
-      const subreddit = await fetchSubredditInfo("test");
-
-      expect(subreddit).toEqual(mockSubredditAbout);
+      expect(subreddits).toEqual(mockSubreddits.data.children);
       expect(fetch).toHaveBeenCalledTimes(1);
-      expect(fetch).toHaveBeenCalledWith(
-        'https://www.reddit.com/r/test/about.json',
-        { method: 'GET' }
-      );
     });
 
     test("handles exceptions with console.error", async () => {
@@ -131,13 +63,69 @@ describe("Fetch Subreddits", () => {
         Promise.reject("API failure")
       );
       const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation();
-      const subreddit = await fetchSubredditInfo("test");
+      const posts = await fetchSubreddits("test");
 
-      expect(subreddit).toEqual(null);
+      expect(posts).toEqual([]);
       expect(consoleErrorSpy).toHaveBeenCalledWith(
-        "Couldn't fetch Subreddit Info: ",
+        "Couldn't fetch Subreddits: ",
         "API failure"
       );
+    });
+
+    describe("Fetch Subreddit Posts", () => {
+      test("should return a listing of posts for a subreddit", async () => {
+        mockFetch(mockPosts);
+        const posts = await fetchSubredditPosts("test");
+
+        expect(posts).toEqual(mockPosts.data.children);
+        expect(fetch).toHaveBeenCalledTimes(1);
+      });
+
+      test("handles exceptions with console.error", async () => {
+        (fetch as jest.Mock).mockImplementationOnce(() =>
+          Promise.reject("API failure")
+        );
+        const consoleErrorSpy = jest
+          .spyOn(console, "error")
+          .mockImplementation();
+        const posts = await fetchSubredditPosts("test");
+
+        expect(posts).toEqual([]);
+        expect(consoleErrorSpy).toHaveBeenCalledWith(
+          "Couldn't fetch Subreddit Posts: ",
+          "API failure"
+        );
+      });
+    });
+
+    describe("Fetch Subreddit Info", () => {
+      test("should return an info object for a subreddit", async () => {
+        mockFetch({ data: mockSubredditAbout });
+        const subreddit = await fetchSubredditInfo("test");
+
+        expect(subreddit).toEqual(mockSubredditAbout);
+        expect(fetch).toHaveBeenCalledTimes(1);
+        expect(fetch).toHaveBeenCalledWith(
+          "https://www.reddit.com/r/test/about.json",
+          { method: "GET" }
+        );
+      });
+
+      test("handles exceptions with console.error", async () => {
+        (fetch as jest.Mock).mockImplementationOnce(() =>
+          Promise.reject("API failure")
+        );
+        const consoleErrorSpy = jest
+          .spyOn(console, "error")
+          .mockImplementation();
+        const subreddit = await fetchSubredditInfo("test");
+
+        expect(subreddit).toEqual(null);
+        expect(consoleErrorSpy).toHaveBeenCalledWith(
+          "Couldn't fetch Subreddit Info: ",
+          "API failure"
+        );
+      });
     });
   });
 });
