9a5e483476d530675a63b9df997139048d8062ad
Robert Baufeld <rbaufeld@posteo.de>
2025-01-06 14:19:56 +0100

    changed jest config and added scss for moduleNameMapper

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'identity-obj-proxy',
-    '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.(css|scss)$': 'identity-obj-proxy',
   },
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   transform: {
