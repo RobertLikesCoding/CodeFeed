05fa41af8f960721f215967d3f1a5348bc71bdab
Robert Baufeld <rbaufeld@posteo.de>
2025-01-10 12:50:14 +0100

    created .module for sideNav, created font variables

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import SubredditsList from "../Subreddits/SubredditsList";
+import styles from "../SideNav/SideNav.module.scss";
 
 const SideNav: React.FC = () => {
   return (
-    <div className="sidenav">
+    <div className={styles.sidenav}>
           <h3>Topics</h3>
           <ul>
             <li className="">
