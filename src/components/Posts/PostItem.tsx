a8c493546c3a9b47e9358897bd55e3a1fe0a782e
Robert Baufeld <rbaufeld@posteo.de>
2025-01-12 17:26:04 +0100

    added basic layout to post component

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -2,35 +2,47 @@ import { useNavigate } from "react-router-dom";
 import UserInfo from "../UserInfo/UserInfo";
 import { Post } from "../services/api/redditAPI";
 import placeHolderImage from "../../assets/placeholder_thumbnail.jpg";
+import styles from "./PostItem.module.scss";
 
 interface PostProps {
-  post: Post['data'];
+  post: Post["data"];
 }
 
-const PostItem: React.FC<PostProps> = ({post}) => {
+const PostItem: React.FC<PostProps> = ({ post }) => {
   const navigate = useNavigate();
 
   async function handleClickPost() {
     if (post.subreddit && post.id) {
       navigate(`/r/${post.subreddit}/${post.id}`);
     } else {
-      navigate(`/r/${post.subreddit}/${post.id}`)
+      navigate(`/r/${post.subreddit}/${post.id}`);
     }
   }
 
   return (
-    <div data-testid="post" onClick={handleClickPost}>
-      <img src={ post.thumbnail === "" ? placeHolderImage : post.thumbnail } alt="thumbnail of post." />
-      <div>
-        <UserInfo author={post.author} created={post.created}/>
-        <h3>{post.title}</h3>
-        <div>
-          <i>{post.ups} upvotes</i>
-          <i>{post.num_comments} comments</i>
+    <div
+      data-testid="post"
+      onClick={handleClickPost}
+      className={`${styles.postContainer}`}
+    >
+      <div className={styles.imgContainer}>
+        <img
+          src={post.thumbnail === "" ? placeHolderImage : post.thumbnail}
+          alt="thumbnail of post."
+        />
+      </div>
+      <div className={styles.content}>
+        <UserInfo author={post.author} created={post.created} />
+        <div className={`${styles.postContent}`}>
+          <h3>{post.title}</h3>
+          <div>
+            <i>{post.ups} upvotes</i>
+            <i>{post.num_comments} comments</i>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default PostItem;
