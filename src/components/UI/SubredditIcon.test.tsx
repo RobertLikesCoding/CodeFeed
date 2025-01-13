24fc8d6b2a48fba9a58cca75388d0d4870642847
Robert Baufeld <rbaufeld@posteo.de>
2025-01-13 23:08:17 +0100

    created tests for SubredditIcon component

---
+++ src/components/UI/SubredditIcon.test.tsx
@@ -0,0 +1,29 @@
+import { screen, render } from "@testing-library/react";
+import SubredditIcon from "./SubredditIcon";
+
+describe("Subreddit ", () => {
+  test("should render small icon when small prop is true", () => {
+    render(<SubredditIcon small={true} color="#FFF" />);
+
+    const icon = screen.getByText("r/");
+
+    expect(icon).toHaveClass("small");
+  });
+
+  test("should render normal icon when small prop is false", () => {
+    render(<SubredditIcon small={false} color="#FFF" />);
+
+    const icon = screen.getByText("r/");
+
+    expect(icon).not.toHaveClass("small");
+
+  });
+
+  test("should render color from props", () => {
+    render(<SubredditIcon small={false} color="#FFF" />);
+
+    const icon = screen.getByText("r/");
+
+    expect(icon).toHaveStyle("backgroundColor: #FFF")
+  });
+});
