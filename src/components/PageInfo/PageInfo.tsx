c4bb7d0fc71533e0994e6351cb3942f5b8f6ddac
Robert Baufeld <rbaufeld@posteo.de>
2025-01-15 13:12:04 +0100

    adjusted Subreddit PageInfo

--- src/components/PageInfo/PageInfo.tsx
+++ src/components/PageInfo/PageInfo.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { SubredditAbout, fetchSubredditInfo } from "../services/api/redditAPI";
-import styles from "../SideNav/SideNav.module.scss";
+import styles from "../PageInfo/PageInfo.module.scss";
 import SubredditsList from "../Subreddits/SubredditsList";
 
 interface Props {
@@ -26,17 +26,24 @@ const PageInfo: React.FC<Props> = ({ topic, subreddit }) => {
         console.error("Error fetching info box: ", error);
       }
     };
-
     fetchPageInfo();
   }, [topic, subreddit]);
 
   if (aboutSubreddit) {
     return (
-      <aside>
+      <aside className={`${styles.about} flex-column gap-1`}>
         <h3>r/{aboutSubreddit.display_name}</h3>
-        <p>Subscribers: {aboutSubreddit.subscribers}</p>
         <p>{aboutSubreddit.public_description}</p>
-        <p>Active Users: {aboutSubreddit.active_user_count}</p>
+        <div className="flex gap-2">
+          <div className="flex-column flex-center">
+            <p>Members:</p>
+            <p>{aboutSubreddit.subscribers}</p>
+          </div>
+          <div className="flex-column flex-center">
+        <p>Online:</p>
+        <p>{aboutSubreddit.active_user_count}</p>
+          </div>
+        </div>
       </aside>
     );
   }
